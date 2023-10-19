c2969fb9ebc3733fa48afe9bd5d743d590847caa
arindranr <145304938+arindranr@users.noreply.github.com>
2023-10-19 11:19:53 +0800

    Posttest5

---
+++ Posttest5/config/functions.php
@@ -0,0 +1,114 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "data_bis");
+
+function tambahBus($data, $gambar)
+{
+    global $conn;
+    $namaBus = $data['nama-bus'];
+    $fasilitas = $data['fasilitas'];
+    $jumlahUnit = $data['jumlah-unit'];
+    $hargaSewa = $data['harga-sewa'];
+
+    $namaGambar = $gambar['name'];
+    $extension = explode(".", $namaGambar);
+    $extension = end($extension);
+    $namaGambar = date("ymdhis") . ".$extension";
+
+    $query = "INSERT INTO bus VALUES (NULL, ?, ?, ?, ?, ?)";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "ssdds", $namaBus, $fasilitas, $jumlahUnit, $hargaSewa, $namaGambar);
+    mysqli_stmt_execute($stmt);
+
+    move_uploaded_file($gambar['tmp_name'], "../admin/gambar-upload/$namaGambar");
+    header('Location: ../admin/index.php');
+}
+
+function updateBus($data, $gambar, $id)
+{
+    global $conn;
+
+    $namaBus = $data['nama-bus'];
+    $fasilitas = $data['fasilitas'];
+    $jumlahUnit = $data['jumlah-unit'];
+    $hargaSewa = $data['harga-sewa'];
+
+    if ($gambar['name'] == "") {
+        $query = "UPDATE bus SET nama_bus = ?, fasilitas = ?, jumlah_unit = ?, harga_sewa = ? WHERE id = ?";
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, "ssddi", $namaBus, $fasilitas, $jumlahUnit, $hargaSewa, $id);
+        mysqli_stmt_execute($stmt);
+    } else {
+        $namaGambar = $gambar['name'];
+        $extension = explode(".", $namaGambar);
+        $extension = end($extension);
+        $namaGambar = date("ymdhis") . ".$extension";
+
+        $namaGambarLama = getDataById($id)[0]['gambar'];
+        unlink("../admin/gambar-upload/$namaGambarLama");
+
+        $query = "UPDATE bus SET nama_bus = ?, fasilitas = ?, jumlah_unit = ?, harga_sewa = ?, gambar = ? WHERE id = ?";
+        $stmt = mysqli_prepare($conn, $query);
+        mysqli_stmt_bind_param($stmt, "ssddsi", $namaBus, $fasilitas, $jumlahUnit, $hargaSewa, $namaGambar, $id);
+        mysqli_stmt_execute($stmt);
+
+        move_uploaded_file($gambar['tmp_name'], "../admin/gambar-upload/$namaGambar");
+
+    }
+
+    header('Location: ../admin/index.php');
+}
+
+function deleteBus($id)
+{
+    global $conn;
+
+    $namaGambar = getDataById($id)[0]['gambar'];
+    unlink("../admin/gambar-upload/$namaGambar");
+
+    $query = "DELETE FROM bus WHERE id = $id";
+    mysqli_query($conn, $query);
+
+    header('Location: ../admin/index.php');
+
+}
+
+function getDataBus()
+{
+    global $conn;
+
+    $query = "SELECT * FROM bus";
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+function getDataById($id)
+{
+    global $conn;
+
+    $query = "SELECT * FROM bus WHERE id = $id";
+    $result = mysqli_query($conn, $query);
+    $rows = [];
+    if ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    return $rows;
+}
+
+if (isset($_POST['tambah-bus'])) {
+    tambahBus($_POST, $_FILES['gambar']);
+}
+
+if (isset($_POST['edit-bus'])) {
+    updateBus($_POST, $_FILES['gambar'], $_GET['id']);
+}
+
+if (isset($_GET['idHapus'])) {
+    deleteBus($_GET['idHapus']);
+}
+
+?>
---
+++ Posttest5/rent.php
@@ -0,0 +1,4 @@
+<?php
+include('layout/header.php');
+include('content/formRent.php');
+include('layout/footer.php');
